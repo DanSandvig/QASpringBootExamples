96ddca909fb78e04747e703b68ebe75434ca7e8e
DanSandvig <dspublicaddress@protonmail.com>
2021-12-13 09:26:59 +0000

    Added default springboot project

---
+++ src/main/java/com/sandvig/firstsignsofsrping/FirstSignsOfSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sandvig.firstsignsofsrping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSignsOfSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSignsOfSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandvig/firstsignsofsrping/FirstSignsOfSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandvig.firstsignsofsrping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSignsOfSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
